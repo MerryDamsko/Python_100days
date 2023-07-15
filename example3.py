a97c274fb4f9fce7197ff8e19ea61d15028b8779
Merry Damsko <merrydamsko@pop-os.localdomain>
2023-07-15 16:12:07 +0200

    example of importing external functions

---
+++ example3.py
@@ -0,0 +1,7 @@
+import string_func
+
+some_string = "Hello, Universe!"
+
+print(string_func.stringLength(some_string))
+print(string_func.stringToLower(some_string))
+print(string_func.stringToUpper(some_string))
---
+++ string_func/example1.py
@@ -0,0 +1,9 @@
+import stringLength
+import stringToLower
+import stringToUpper
+
+some_string = "Hello, Universe"
+
+print(stringLength.stringLength(some_string))
+print(stringToLower.stringToLower(some_string))
+print(stringToUpper.stringToUpper(some_string))
